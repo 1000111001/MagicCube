6156a4e4bc988d80a58a5bf7a85a6b1d04a615c4
hongxiaofeng <1494467667@qq.com>
2023-09-23 22:28:27 +0800

    LogicCube

--- src/model/logic-cube.js
+++ src/model/logic-cube.js
@@ -1,3 +1,5 @@
+import { matIV } from ".";
+
 class LogicBlock {
     constructor () {
         this.index = {x: 0, y: 0, z: 0};
@@ -25,6 +27,13 @@ export class LogicCube {
 				}
 			}
 		}
+
+        this.colorTransL = [0, 4, 5, 2];
+        this.colorTransR = Array.from(this.colorTransL).reverse();
+        this.colorTransU = [4, 1, 2, 3];
+        this.colorTransD = Array.from(this.colorTransU).reverse();
+        this.colorTransF = [0, 1, 5, 3];
+        this.colorTransB = Array.from(this.colorTransF).reverse();
     }
 
     ToArray() {
@@ -34,36 +43,85 @@ export class LogicCube {
             return newArr;
         }
         let data = [];
-        data.push(to2D(this.blocks.filter(b => b.position.y == 1).map(b => b.colors[0])));
-        data.push(to2D(this.blocks.filter(b => b.position.x == -1).map(b => b.colors[1])));
-        data.push(to2D(this.blocks.filter(b => b.position.z == 1).map(b => b.colors[2])));
-        data.push(to2D(this.blocks.filter(b => b.position.x == 1).map(b => b.colors[3])));
-        data.push(to2D(this.blocks.filter(b => b.position.z == -1).map(b => b.colors[4])));
-        data.push(to2D(this.blocks.filter(b => b.position.y == -1).map(b => b.colors[5])));
+        data.push(to2D(this.blocks.filter(b => b.position.y == 1).sort((a, b) => {
+            return a.position.z != b.position.z ? a.position.z - b.position.z : a.position.x - b.position.x;
+        }).map(b => b.colors[0])));
+        data.push(to2D(this.blocks.filter(b => b.position.x == -1).sort((a, b) => {
+            return a.position.y != b.position.y ? b.position.y - a.position.y : a.position.z - b.position.z;
+        }).map(b => b.colors[1])));
+        data.push(to2D(this.blocks.filter(b => b.position.z == 1).sort((a, b) => {
+            return a.position.y != b.position.y ? b.position.y - a.position.y : a.position.x - b.position.x;
+        }).map(b => b.colors[2])));
+        data.push(to2D(this.blocks.filter(b => b.position.x == 1).sort((a, b) => {
+            return a.position.y != b.position.y ? b.position.y - a.position.y : b.position.z - a.position.z;
+        }).map(b => b.colors[3])));
+        data.push(to2D(this.blocks.filter(b => b.position.z == -1).sort((a, b) => {
+            return a.position.y != b.position.y ? b.position.y - a.position.y : b.position.x - a.position.x;
+        }).map(b => b.colors[4])));
+        data.push(to2D(this.blocks.filter(b => b.position.y == -1).sort((a, b) => {
+            return a.position.z != b.position.z ? a.position.z - b.position.z : a.position.x - b.position.x;
+        }).map(b => b.colors[5])));
         return data;
     }
 
-    L() {
-        
-    }
-    
-    R() {
-        
-    }
-    
-    F() {
-        
+    OnRotate(normal, r) {
+        let clockwise = r > 0;
+        let colorTrans;
+        console.log(normal);
+        if (normal[0] == -1) {
+            colorTrans = clockwise ? this.colorTransL : this.colorTransR;
+        } 
+        else if (normal[0] == 1) {
+            colorTrans = clockwise ? this.colorTransR: this.colorTransL;
+        }
+        else if (normal[2] == 1) {
+            colorTrans = clockwise ? this.colorTransF: this.colorTransB;
+        }
+        else if (normal[2] == -1) {
+            colorTrans = clockwise ? this.colorTransB: this.colorTransF;
+        }
+        else if (normal[1] == 1) {
+            colorTrans = clockwise ? this.colorTransU: this.colorTransD;
+        }
+        else if (normal[1] == -1) {
+            colorTrans = clockwise ? this.colorTransD: this.colorTransU;
+        }
+        this.Rot(clockwise, normal, b => {
+            if (normal[0] != 0) return b.position.x == normal[0];
+            else if (normal[1] != 0) return b.position.y == normal[1];
+            else if (normal[2] != 0) return b.position.z == normal[2];
+        }, colorTrans);
     }
 
-    B() {
-        
+    L() { this.Rot(true, [-1, 0, 0], b => b.position.x == -1, this.colorTransL); }
+    R() { this.Rot(true, [1, 0, 0], b => b.position.x == 1, this.colorTransR); }
+    F() { this.Rot(true, [0, 0, 1], b => b.position.z == 1, this.colorTransF); }
+    B() { this.Rot(true, [0, 0, -1], b => b.position.z == -1, this.colorTransB); }
+    U() { this.Rot(true, [0, 1, 0], b => b.position.y == 1, this.colorTransU); }
+    D() { this.Rot(true, [0, -1, 0], b => b.position.y == -1, this.colorTransD); }
+
+    Rot(clockwise, axis, filter, colorTrans) {
+        let radius = clockwise ? -0.5 * Math.PI : 0.5 * Math.PI; 
+        let mat = matIV.identity();
+        matIV.rotate(mat, radius, axis, mat);
+        let list = this.blocks.filter(filter);
+        for (let i = 0; i < list.length; i++) {
+            const b = list[i];
+            const p = [b.position.x, b.position.y, b.position.z];
+            p = matIV.multiplyVec3(mat, p);
+            b.position.x = Math.round(p[0]);
+            b.position.y = Math.round(p[1]);
+            b.position.z = Math.round(p[2]);
+            this.TransColor(b.colors, colorTrans);
+        }
     }
 
-    U() {
-
-    }
-
-    D() {
-
+    TransColor(colors, trans) {
+        let c = colors[trans[0]];
+        for (let i = 0; i < trans.length - 1; ++i) {
+            let next = (i + 1) % trans.length;
+            colors[trans[i]] = colors[trans[next]];
+        }
+        colors[trans[trans.length - 1]] = c;
     }
 }
