0c3b5ec54e49c8e305139428f0ed43ba2ab69f2b
hongxiaofeng <1494467667@qq.com>
2023-10-10 01:52:16 +0800

    solve

--- src/model/logic-cube.ts
+++ src/model/logic-cube.ts
@@ -1,12 +1,10 @@
 import { matIV } from ".";
 
 class LogicBlock {
-    index: any;
     position: any;
     colors: Array<number>;
 
     constructor () {
-        this.index = {x: 0, y: 0, z: 0};
         this.position = {x: 0, y: 0, z: 0};
         this.colors = [0, 0, 0, 0, 0, 0];
     }
@@ -40,7 +38,6 @@ export class LogicCube {
 			for (let y = -1; y <= 1; ++y) {
 				for (let z = -1; z <= 1; ++z) {
 					let cube = new LogicBlock();
-                    cube.index = {x, y, z};
                     cube.position = {x, y, z};
 					if (y == 1) cube.colors[0] = 1;
 					if (x == -1) cube.colors[1] = 2;
@@ -83,7 +80,7 @@ export class LogicCube {
             }
         }
         // D
-        for (let j = 8; j >= 6; --j) {
+        for (let j = 6; j < 9; ++j) {
             for (let i = 0; i < 3; ++i) {
                 str += LogicCube.colorName.get(data[5][j - 6][i]);
             }
@@ -104,30 +101,53 @@ export class LogicCube {
     }
 
     ToArray() {
+        function dot(a: {x:number,y:number,z:number}, b: {x:number,y:number,z:number}) {
+            return a.x*b.x + a.y*b.y + a.z*b.z;
+        }
+        function filter(c: {x:number,y:number,z:number}, b: LogicBlock) {
+            if (c.x == c.y) {
+                return b.position.z == c.z;
+            }
+            else if (c.x == c.z) {
+                return b.position.y == c.y;
+            }
+            return b.position.x == c.x;
+        }
         function to2D(arr: Array<any>) {
             const newArr = [];
             while(arr.length) newArr.push(arr.splice(0,3));
             return newArr;
         }
+        function findBlocks(blocks: LogicBlock[], front:LogicBlock, up:LogicBlock, right:LogicBlock):any[][] {
+            return to2D(blocks.filter(b => filter(front.position, b)).sort((a, b) => {
+                return dot(a.position, up.position) != dot(b.position, up.position) 
+                    ? dot(b.position, up.position) - dot(a.position, up.position)
+                    : dot(a.position, right.position) - dot(b.position, right.position);
+            }).map(b => b.colors[front.colors.indexOf(Math.max(...front.colors))]))
+        }
+        let centers: LogicBlock[] = [];
+        for (let i = 0; i < this.blocks.length; ++i) {
+            let b = this.blocks[i];
+            let zeroCnt = 0;
+            zeroCnt += (b.position.x == 0 ? 1 : 0);
+            zeroCnt += (b.position.y == 0 ? 1 : 0);
+            zeroCnt += (b.position.z == 0 ? 1 : 0);
+            if (zeroCnt != 2) continue;
+            centers[Math.max(...b.colors) - 1] = b;
+        }
+        let up = centers[0];
+        let left = centers[1];
+        let front = centers[2];
+        let right = centers[3];
+        let back = centers[4];
+        let down = centers[5];
         let data = [];
-        data.push(to2D(this.blocks.filter(b => b.position.y == 1).sort((a, b) => {
-            return a.position.z != b.position.z ? a.position.z - b.position.z : a.position.x - b.position.x;
-        }).map(b => b.colors[0])));
-        data.push(to2D(this.blocks.filter(b => b.position.x == -1).sort((a, b) => {
-            return a.position.y != b.position.y ? b.position.y - a.position.y : a.position.z - b.position.z;
-        }).map(b => b.colors[1])));
-        data.push(to2D(this.blocks.filter(b => b.position.z == 1).sort((a, b) => {
-            return a.position.y != b.position.y ? b.position.y - a.position.y : a.position.x - b.position.x;
-        }).map(b => b.colors[2])));
-        data.push(to2D(this.blocks.filter(b => b.position.x == 1).sort((a, b) => {
-            return a.position.y != b.position.y ? b.position.y - a.position.y : b.position.z - a.position.z;
-        }).map(b => b.colors[3])));
-        data.push(to2D(this.blocks.filter(b => b.position.z == -1).sort((a, b) => {
-            return a.position.y != b.position.y ? b.position.y - a.position.y : b.position.x - a.position.x;
-        }).map(b => b.colors[4])));
-        data.push(to2D(this.blocks.filter(b => b.position.y == -1).sort((a, b) => {
-            return a.position.z != b.position.z ? a.position.z - b.position.z : a.position.x - b.position.x;
-        }).map(b => b.colors[5])));
+        data.push(findBlocks(this.blocks, up, back, right));
+        data.push(findBlocks(this.blocks, left, up, front));
+        data.push(findBlocks(this.blocks, front, up, right));
+        data.push(findBlocks(this.blocks, right, up, back));
+        data.push(findBlocks(this.blocks, back, up, left));
+        data.push(findBlocks(this.blocks, down, front, right));
         return data;
     }
 
--- src/model/magic-cube.js
+++ src/model/magic-cube.js
@@ -57,6 +57,17 @@ export class MagicCube {
 				}
 			}
 		}
+		this.upCenter = getCube(this, 1, 2, 1);
+		this.rightCenter = getCube(this, 2, 1, 1);
+		this.frontCenter = getCube(this, 1, 1, 2);
+		this.downCenter = getCube(this, 1, 0, 1);
+		this.leftCenter = getCube(this, 0, 1, 1);
+		this.backCenter = getCube(this, 1, 1, 0);
+		this.centers
+
+		function getCube(self, x, y, z) {
+			return self.cubes[3*(3*x + y) + z]
+		}
 	}
 
 	rotateGroup(group, normal, angle) {
@@ -104,28 +115,31 @@ export class MagicCube {
 		})();
 	}
 	
-    L() { this.Rot(0, 0, 90); }
-    R() { this.Rot(0, 2, 90); }
-    F() { this.Rot(2, 2, 90); }
-    B() { this.Rot(2, 0, 90); }
-    U() { this.Rot(1, 2, 90); }
-    D() { this.Rot(1, 0, 90); }
+    L() { this.Rot(this.leftCenter, 90); }
+    R() { this.Rot(this.rightCenter, 90); }
+    F() { this.Rot(this.frontCenter, 90); }
+    B() { this.Rot(this.backCenter, 90); }
+    U() { this.Rot(this.upCenter, 90); }
+    D() { this.Rot(this.downCenter, 90); }
 	
-    Li() { this.Rot(0, 0, -90); }
-    Ri() { this.Rot(0, 2, -90); }
-    Fi() { this.Rot(2, 2, -90); }
-    Bi() { this.Rot(2, 0, -90); }
-    Ui() { this.Rot(1, 2, -90); }
-    Di() { this.Rot(1, 0, -90); }
+    Li() { this.Rot(this.leftCenter, -90); }
+    Ri() { this.Rot(this.rightCenter, -90); }
+    Fi() { this.Rot(this.frontCenter, -90); }
+    Bi() { this.Rot(this.backCenter, -90); }
+    Ui() { this.Rot(this.upCenter, -90); }
+    Di() { this.Rot(this.downCenter, -90); }
 	
-    L2() { this.Rot(0, 0, 180); }
-    R2() { this.Rot(0, 2, 180); }
-    F2() { this.Rot(2, 2, 180); }
-    B2() { this.Rot(2, 0, 180); }
-    U2() { this.Rot(1, 2, 180); }
-    D2() { this.Rot(1, 0, 180); }
+    L2() { this.Rot(this.leftCenter, 180); }
+    R2() { this.Rot(this.rightCenter, 180); }
+    F2() { this.Rot(this.frontCenter, 180); }
+    B2() { this.Rot(this.backCenter, 180); }
+    U2() { this.Rot(this.upCenter, 180); }
+    D2() { this.Rot(this.downCenter, 180); }
 
-	Rot(axis, j, degree) {
+	Rot(center, degree) {
+		let id = center.id.map(x => Math.abs(x - 1));
+		let axis = id.indexOf(Math.max(...id));
+		let j = center.id[axis];
 		const g = []
 		for (let i = 0; i < this.cubes.length; ++i) {
 			if (this.cubes[i].id[axis] == j) g.push(this.cubes[i]);
